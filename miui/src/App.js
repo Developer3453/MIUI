b6c64a425a1cd07f3640a92e57a433705a7726be
Developer3453 <manojiviswa10@gmail.com>
2023-01-05 17:55:32 +0530

    Changed

---
+++ miui/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {Typography, AppBar, Toolbar, Tabs, Tab} from "@mui/material";
+
+
+
+function App() {
+  return (
+    <>
+    <AppBar>
+      
+      <Toolbar>
+        <Tabs sx={{marginLeft: "auto"}}>
+          <Tab label="Home" />
+          <Tab label="About" />
+          <Tab label="Contact" />
+
+        </Tabs>
+
+      </Toolbar>
+    </AppBar>
+  
+    
+    
+  
+  
+
+     
+      
+
+    
+
+
+      
+       </>
+  );
+}
+
+export default App;
